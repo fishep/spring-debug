23064eb3e9dea65ee0dece17c641d7265032faac
fishep <fishep@qq.com>
2024-02-24 11:03:27 +0800

    first commit

---
+++ src/main/java/com/fishep/spring/debug/SpringDebugApplication.java
@@ -0,0 +1,13 @@
+package com.fishep.spring.debug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDebugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDebugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fishep/spring/debug/SpringDebugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishep.spring.debug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDebugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
